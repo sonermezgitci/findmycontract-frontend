48acc96ada922728e204583825010a1925e249cf
Soner Mezgitci <mezgitci9@gmail.com>
2020-09-07 23:46:02 -0400

    fetch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 
 class App extends React.Component {
+ componentDidMount (){
+  fetch ('http://localhost:3000/contracts',{
+    
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+
+ }
+
+
+
   render() {
     return (
       <div className="App">
