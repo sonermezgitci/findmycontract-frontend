0d971c76309815a3ea9a427ad3f45109509cceac
Soner Mezgitci <mezgitci9@gmail.com>
2020-09-08 15:17:14 -0400

    imported packcages into index.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React from 'react';
 
 
 class App extends React.Component {
- componentDidMount (){
-  fetch ('http://localhost:3000/contracts',{
-    
-  })
-  .then(response => response.json())
-  .then(data => console.log(data))
-
- }
+ 
+ 
+ 
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {createStore} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 import App from './App';
+
+
+// set up our  store 
+
 
 
 ReactDOM.render(
