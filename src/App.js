4fd7b53b1813fdcb6c99deefdea519cfbf2a2215
Soner Mezgitci <mezgitci9@gmail.com>
2020-09-08 20:59:59 -0400

    set up index.js and created our store

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import React from 'react';
 
 class App extends React.Component {
  
- 
- 
+  componentDidMount (){
+  fetch ('http://localhost:3000/players',{
+    
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+
+ }
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import App from './App';
 
+ 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// set up our  store 
+
+let store = createStore(reducer, composeEnchancers(applyMiddleware(thunk)))
+
 
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+   <App />
+  </Provider>,
+  
   document.getElementById('root')
 );
 
