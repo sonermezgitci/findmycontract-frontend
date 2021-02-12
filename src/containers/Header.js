44d65410134faa612a5bff55a243f52fdd2eae33
Soner Mezgitci <mezgitci9@gmail.com>
2021-02-12 07:48:44 -0500

    headeroption css

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import "./Header.css";
-import HeaderOption from "./containers/HeaderOption";
+import HeaderOption from "./HeaderOption";
 
 
 function Header() {
     return (
         <div className="header">
 
+
             <div className="header__left" >
+                {/* <h1> FIND MY CONTRACT </h1> */}
                 <img src="https://theundefeated.com/wp-content/uploads/2017/06/nbalogo.jpg?w=1024" alt="" />
-                <h1> FIND MY CONTRACT </h1>
-            </div>
-            <div className="header_right">
-                <HeaderOption title="home" />
-                <HeaderOption email="home" />
-                <HeaderOption subscribe="subscribe" />
+                <div className="header__right">
+                    <HeaderOption title="home" />
+                    <HeaderOption title="contact" />
+                    <HeaderOption title="about" />
+                </div>
+
+
 
             </div>
         </div>
--- src/containers/HeaderOption.js
+++ src/containers/HeaderOption.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import './HeaderOption.css'
+import React from 'react';
+import './HeaderOption.css';
 
 
 function HeaderOption({ Icon, title }) {
